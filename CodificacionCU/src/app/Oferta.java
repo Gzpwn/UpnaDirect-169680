c352842aefe2b9acaaa72c336d6027143f5b870e
= <ikerpg2112@gmail.com>
2025-12-19 22:42:52 +0100

    Oferta v1

--- CodificacionCU/src/app/Oferta.java
+++ CodificacionCU/src/app/Oferta.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package app;
 
-/**
- *
- * @author alumno
- */
+
 public class Oferta {
+    
+    private String aseguradora;
+    private int poliza;
+    private Cliente cliente;
+    private Bien bien;
+    private int comision;
+    
+    public Oferta(String aseguradora,int poliza, Cliente cliente, Bien bien) {
+        
+        this.aseguradora = aseguradora;
+        this.poliza = poliza;
+        this.cliente = cliente;
+        this.bien = bien;
+        this.comision = 0;
+        
+    }
+    
+    public void anadirComision(int comision) {  
+        this.comision = comision; 
+    }
+    
+    public int obtenerComision() {
+        return this.comision;
+    }
+    
+    public String obtenerAseguradora() {
+        return this.aseguradora;
+    }
+    
+    public int obtenerPoliza() { 
+        return this.poliza;
+    }
+    
+    public Cliente obtenerCliente() {
+        return this.cliente;
+    }
+    
+    public Bien obtenerBien() {
+        return this.bien;
+    }
     
 }
