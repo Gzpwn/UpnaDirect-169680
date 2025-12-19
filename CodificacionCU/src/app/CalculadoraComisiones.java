248790b3948e56deac207b69ebdbff745c273088
= <ikerpg2112@gmail.com>
2025-12-19 22:13:55 +0100

    Calculadora de comisiones terminada

--- CodificacionCU/src/app/CalculadoraComisiones.java
+++ CodificacionCU/src/app/CalculadoraComisiones.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package app;
 
-/**
- *
- * @author alumno
- */
 public class CalculadoraComisiones {
+    
+    
+    
+    public int calcularComision(String aseguradora, int importeSeguro) {
+    
+        aseguradora = aseguradora.toLowerCase();
+        if (importeSeguro < 1000) {
+            return (int)(importeSeguro*0.01);
+        } else {
+            
+            if (aseguradora.equals("mafro")) {
+                
+                return (int)(importeSeguro*0.03);
+            } else if (aseguradora.equals("linea indirecta")) {
+                return ((int)(importeSeguro*0.04));
+            } else {
+                
+                return (int)(importeSeguro*0.05);
+            }
+        }
+        
+    }
+    
+ 
+    
     
 }
