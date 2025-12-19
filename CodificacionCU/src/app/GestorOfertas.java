a186aa14b8a7fbd2a25820c9cdee6b221962634d
= <ikerpg2112@gmail.com>
2025-12-19 22:59:06 +0100

    Gestor v1

--- CodificacionCU/src/app/GestorOfertas.java
+++ CodificacionCU/src/app/GestorOfertas.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package app;
 
-/**
- *
- * @author alumno
- */
+import java.util.List;
+import java.util.ArrayList;
+
 public class GestorOfertas {
     
+    Private List<Oferta> ofertas = new ArrayList<>();
+    public GestorOfertas(List<Oferta> ofertas) {
+        
+        this.ofertas = ofertas;
+        
+        
+    }
+    
+    
+    public Oferta mejorOferta() {
+        
+    }
 }
