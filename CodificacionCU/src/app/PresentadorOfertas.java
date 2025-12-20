ff7795ee0028ec004653411dff61439aa6a122a3
Iker <ikerpg2112@gmail.com>
2025-12-20 21:39:10 +0100

    Presentador v1

---
+++ CodificacionCU/src/app/PresentadorOfertas.java
@@ -0,0 +1,28 @@
+
+package app;
+
+
+public class PresentadorOfertas {
+    
+    private Vista vista;
+    private Sistema sistema;
+    
+    public PresentadorOfertas(Vista vista) {
+        
+        this.vista = vista;
+        this.sistema = new Sistema();
+    }
+    
+    public void solicitarMejOferta(int fechaNacimiento, int salario, String tipoBien, int valorBien) {
+ 
+        
+        Oferta oferta = sistema.ofrecerMejorOferta(fechaNacimiento, salario, tipoBien, valorBien);
+        if (oferta == null) { // Si la oferta no es válida, llamamos a la vista para que muestre por pantalla el mensaje de error
+            vista.mostrarError("\n[!] No se ha podido calcular la oferta\n");
+        } else {
+            vista.mostrarMejorOferta(oferta); // Si la oferta es válida, llamamos a la vista para que muestre por pantalla la oferta
+            
+        }
+    }
+    
+}
