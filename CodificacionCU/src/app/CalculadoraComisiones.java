939c25ae09a5adfa4f947cbfa167ab9eeb406c40
iker <ikerpg2112@gmail.com>
2025-12-22 12:27:07 +0100

    Calculadora comisiones final

--- CodificacionCU/src/app/CalculadoraComisiones.java
+++ CodificacionCU/src/app/CalculadoraComisiones.java
@@ -5,7 +5,7 @@ public class CalculadoraComisiones {
     
     
     
-    public int calcularComision(String aseguradora, int importeSeguro) {
+    public int calcularComision(String aseguradora, int importeSeguro) { // Funcion para calcular las comisiones de los seguros
     
         aseguradora = aseguradora.toLowerCase();
         if (importeSeguro < 1000) {
@@ -17,9 +17,13 @@ public class CalculadoraComisiones {
                 return (int)(importeSeguro*0.03);
             } else if (aseguradora.equals("linea indirecta")) {
                 return ((int)(importeSeguro*0.04));
-            } else {
+                
+            } else  if (aseguradora.equals("adasles")) {
                 
                 return (int)(importeSeguro*0.05);
+            } else {
+                
+                return 0;
             }
         }
         
