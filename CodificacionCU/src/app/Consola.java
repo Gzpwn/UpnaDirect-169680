d0cc1d9da1b90c71d676b36b9ae08d4cf6477b3d
iker <ikerpg2112@gmail.com>
2025-12-22 06:59:31 +0100

    Consola v2

--- CodificacionCU/src/app/Consola.java
+++ CodificacionCU/src/app/Consola.java
@@ -32,23 +32,44 @@ public class Consola implements Vista{
     }
     
     public void iniciarConsola() {
-        System.out.println("----- CORREDURÍA DE SEGUROS UPNA DIRECT -----");
         
-        System.out.println("[i] Introduce tu fecha de nacimiento: ");
-        int fechaNacimiento = escaner.nextInt(); // Almacenamos la fecha de nacimiento
+        Oferta oferta = null;
         
-        System.out.println("[i] Introduce tu salario anual: ");
-        int salarioAnual = escaner.nextInt(); // Almacenamos el salario anual
+        while (oferta == null) {
+            
+            System.out.println("----- CORREDURÍA DE SEGUROS UPNA DIRECT -----");
         
-        escaner.nextLine(); // Limpiamos el buffer para recoger el resto de datos
+            System.out.println("[i] Introduce tu fecha de nacimiento: ");
+            int fechaNacimiento = escaner.nextInt(); // Almacenamos la fecha de nacimiento
+
+            System.out.println("[i] Introduce tu salario anual: ");
+            int salarioAnual = escaner.nextInt(); // Almacenamos el salario anual
+
+            escaner.nextLine(); // Limpiamos el buffer para recoger el resto de datos
+
+            System.out.println("[i] Introduce el tipo de bien que tienes (vivienda/vehículo): ");
+            String tipoBien = escaner.nextLine(); // Almacenamos el tipo de bien
+
+            System.out.println("[i] Introduce el valor del bien que has indicado: ");
+            int valorBien = escaner.nextInt(); // Almacenamos el valor del bien
+
+            oferta = presentador.devolverMejorOferta(fechaNacimiento, valorBien, tipoBien, valorBien); // Obtenemos la mejor oferta
+
+            if (oferta == null) {
+                System.out.println("\n[!] Datos incorrectos, vuelve a introducirlos\n");
+            }
+
+  
+        }
         
-        System.out.println("[i] Introduce el tipo de bien que tienes (vivienda/vehículo): ");
-        String tipoBien = escaner.nextLine(); // Almacenamos el tipo de bien
+        mostrarOferta(oferta);
         
-        System.out.println("[i] Introduce el valor del bien que has indicado: ");
-        int valorBien = escaner.nextInt(); // Almacenamos el valor del bien
         
-        presentador.solicitarMejOferta(fechaNacimiento, valorBien, tipoBien, valorBien); // Obtenemos la mejor oferta
+    }
+    
+    public void mostrarOferta(Oferta oferta) {
+        
+        System.out.println(oferta.obtenerAseguradora().toUpperCase() + " | " + oferta.obtenerPoliza() + " | " + oferta.obtenerComision());
         
     }
 }
